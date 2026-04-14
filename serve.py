83b5afb7de786a6b547127aa9cd125af5d45a72e
Remik202 <olegantr.zdamar@gmail.com>
2026-04-14 13:09:25 +0300

    add play, pause, Volume, Mute, Fullscreen buttons

---
+++ serve.py
@@ -0,0 +1,23 @@
+from livereload import Server
+
+
+DEFAULT_PORT = 5500
+DEFAULT_PAGE = "index.html"
+
+
+def main():
+    server = Server()
+    server.watch("*.html")
+    server.watch("*.js")
+    server.watch("*.css")
+    server.watch("static/*.css")
+    server.watch("static/*.js")
+    server.serve(
+        root=".",
+        port=DEFAULT_PORT,
+        default_filename=DEFAULT_PAGE,
+    )
+
+
+if __name__ == "__main__":
+    main()
